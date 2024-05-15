b4a8c55fa5c05c444b876278f962c574de488f8b
Dyandra Aurellia Agata Fitri <160992091+dyandraaurellia@users.noreply.github.com>
2024-05-15 17:20:20 +0700

    Add files via upload

---
+++ Java_Exception Handling/LATIHAN/unCheckException.java
@@ -0,0 +1,15 @@
+package LATIHAN;
+
+public class unCheckException {
+    public static void main(String arg[]){
+        try{
+            int []arr = new int[1];
+            System.out.println(arr[1]);
+            System.out.println("Baris ini tidak pernah dieksekusi");
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Terjadi eksepsi karena index melebihi kapasitas");
+            } 
+            System.out.println("Setelah blok try-catch");
+          }
+        }
+
